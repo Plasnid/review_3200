66c7307bb290a573374df7aee09a8ef3b6a6ab45
Plasnid <stuart@sackler.ca>
2020-08-20 23:25:01 -0400

    updated the level of detail in booleans.

---
+++ datastructures.js
@@ -0,0 +1,5 @@
+/**
+ * ! Datastructures
+ * * By using a combination of some/all of these concepts together
+ * * we can make bigger structures.  Lets build a list of awesome terms!
+ */
--- datatypes/booleans.js
+++ datatypes/booleans.js
@@ -2,6 +2,14 @@
  * * So far we have strings and numbers.  Lets move on to Booleans
  * * Booleans can either be true or false
  * 
+ * * False is based on the idea that some values are always viewed by 
+ * * javaScript as not true.  The general term for this is falsy.
+ *  
+ * * The following values are falsy:
+ * * false, 0, ''(which is an empty string), null, undefined, and NaN(which stands from not a number)
+ * 
+ * * Most of the time for this semester we will stick to false
+ * 
  * * Lets try out an example, by checking if flamingoes are native to canada
  * * Sadly they are not, so we have to say false...a good variable name...let see
  * * How about flamingoesAreCanadian?  That works, and it is camel case :D
